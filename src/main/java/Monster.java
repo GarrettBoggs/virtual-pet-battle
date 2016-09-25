35fc6b25f148463834dcb69992561bd96c72ee8c
Matt Nicholas <fostermatt82@gmial.com>
2016-09-25 16:12:20 -0700

    add constant variables for play, food and sleep to moster class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -4,13 +4,26 @@ import java.util.ArrayList;
 import org.sql2o.*;
 
 public class Monster {
-    public int id;
+    private int id;
     private String name;
     private int personId;
+    private int foodLevel;
+    private int sleepLevel;
+    private int playLevel;
+
+    public static final int MAX_FOOD_LEVEL = 3;
+    public static final int MAX_SLEEP_LEVEL = 8;
+    public static final int MAX_PLAY_LEVEL = 12;
+    public static final int MIN_ALL_LEVELS = 0;
+
 
     public Monster(String name, int personId){
         this.name = name;
         this.personId = personId;
+        playLevel = MAX_PLAY_LEVEL / 2;
+        sleepLevel = MAX_SLEEP_LEVEL / 2;
+        foodLevel = MAX_FOOD_LEVEL / 2;
+
     }
     // Getters
     public int getId(){
@@ -22,6 +35,17 @@ public class Monster {
     public int getPersonId(){
         return personId;
     }
+    public int getPlayLevel(){
+    return playLevel;
+  }
+  public int getSleepLevel(){
+      return sleepLevel;
+    }
+    public int getFoodLevel(){
+    return foodLevel;
+  }
+
+
 
     @Override
     public boolean equals(Object otherMonster){
@@ -58,4 +82,40 @@ public class Monster {
             return monster;
         }
     }
+    public boolean isAlive() {
+    if (foodLevel <= MIN_ALL_LEVELS ||
+    playLevel <= MIN_ALL_LEVELS ||
+    sleepLevel <= MIN_ALL_LEVELS) {
+      return false;
+    }
+    return true;
+  }
+  public void depleteLevels(){
+    playLevel--;
+    foodLevel--;
+    sleepLevel--;
+  }
+  public void play(){
+    if (playLevel >= MAX_PLAY_LEVEL){
+      throw new UnsupportedOperationException("You cannot play with monster anymore!");
+    }
+    playLevel++;
+  }
+    public void sleep(){
+        if (sleepLevel >= MAX_SLEEP_LEVEL){
+          throw new UnsupportedOperationException("You cannot make your monster sleep anymore!");
+        }
+        sleepLevel++;
+      }
+  public void feed(){
+    if (foodLevel >= MAX_FOOD_LEVEL){
+      throw new UnsupportedOperationException("You cannot feed your monster anymore!");
+    }
+    foodLevel++;
+  }
+
+
+
+
+
 }
